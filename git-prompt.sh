857eb131e544fd8a93be21e214b2aff3799b41d6
Brian Landau <brianjlandau@gmail.com>
2010-08-30 14:34:11 -0400

    Customizations

--- git-prompt.sh
+++ git-prompt.sh
@@ -9,21 +9,14 @@
         unset modified_vcs_color added_vcs_color addmoded_vcs_color untracked_vcs_color op_vcs_color detached_vcs_color hex_vcs_color
         unset rawhex_len
 
-        conf=git-prompt.conf;                   [[ -r $conf ]]  && . $conf
-        conf=/etc/git-prompt.conf;              [[ -r $conf ]]  && . $conf
         conf=~/.git-prompt.conf;                [[ -r $conf ]]  && . $conf
-        conf=~/.config/git-prompt.conf;         [[ -r $conf ]]  && . $conf
         unset conf
 
 
         #####  set defaults if not set
 
         git_module=${git_module:-on}
-        svn_module=${svn_module:-off}
-        hg_module=${hg_module:-on}
-        vim_module=${vim_module:-on}
         virtualenv_module=${virtualenv_module:-on}
-        error_bell=${error_bell:-off}
         cwd_cmd=${cwd_cmd:-\\w}
 
 
@@ -64,16 +57,12 @@
         count_only=${count_only:-off}
         rawhex_len=${rawhex_len:-5}
 
-        aj_max=20
-
 
 #####################################################################  post config
 
         ################# make PARSE_VCS_STATUS
         unset PARSE_VCS_STATUS
         [[ $git_module = "on" ]]   &&   type git >&/dev/null   &&   PARSE_VCS_STATUS+="parse_git_status"
-        [[ $svn_module = "on" ]]   &&   type svn >&/dev/null   &&   PARSE_VCS_STATUS+="${PARSE_VCS_STATUS+||}parse_svn_status"
-        [[ $hg_module  = "on" ]]   &&   type hg  >&/dev/null   &&   PARSE_VCS_STATUS+="${PARSE_VCS_STATUS+||}parse_hg_status"
                                                                     PARSE_VCS_STATUS+="${PARSE_VCS_STATUS+||}return"
         ################# terminfo colors-16
         #
@@ -126,8 +115,11 @@
 
         on=''
         off=': '
-        bell="\[`eval ${!error_bell} tput bel`\]"
         colors_reset='\['`tput sgr0`'\]'
+				
+				##################################################################### 
+				# if label non empty, append 1 space
+				label=${1:+$1 }
 
         # replace symbolic colors names to raw treminfo strings
                  init_vcs_color=${!init_vcs_color}
@@ -156,8 +148,6 @@
         else
                 elipses_marker="..."
         fi
-
-        export who_where
 
 
 cwd_truncate() {
@@ -219,194 +209,11 @@ cwd_truncate() {
         return
  }
 
-
-set_shell_label() {
-
-        xterm_label() {
-                local args="$*"
-                echo  -n "]2;${args:0:200}" ;    # FIXME: replace hardcodes with terminfo codes
-        }
-
-        screen_label() {
-                # FIXME: run this only if screen is in xterm (how to test for this?)
-                xterm_label  "$plain_who_where $@"
-
-                # FIXME $STY not inherited though "su -"
-                [ "$STY" ] && screen -S $STY -X title "$*"
-        }
-        if [[ -n "$STY" ]]; then
-                screen_label "$*"
-        else
-                case $TERM in
-
-                        screen*)
-                                screen_label "$*"
-                                ;;
-
-                        xterm* | rxvt* | gnome-* | konsole | eterm | wterm )
-                                # is there a capability which we can to test
-                                # for "set term title-bar" and its escapes?
-                                xterm_label  "$plain_who_where $@"
-                                ;;
-
-                        *)
-                                ;;
-                esac
-        fi
- }
-
-    export -f set_shell_label
-
-###################################################### ID (user name)
-        id=`id -un`
-        id=${id#$default_user}
-
-########################################################### TTY
-        tty=`tty`
-        tty=`echo $tty | sed "s:/dev/pts/:p:; s:/dev/tty::" `           # RH tty devs
-        tty=`echo $tty | sed "s:/dev/vc/:vc:" `                         # gentoo tty devs
-
-        if [[ "$TERM" = "screen" ]] ;  then
-
-                #       [ "$WINDOW" = "" ] && WINDOW="?"
-                #
-                #               # if under screen then make tty name look like s1-p2
-                #               # tty="${WINDOW:+s}$WINDOW${WINDOW:+-}$tty"
-                #       tty="${WINDOW:+s}$WINDOW"  # replace tty name with screen number
-                tty="$WINDOW"  # replace tty name with screen number
-        fi
-
-        # we don't need tty name under X11
-        case $TERM in
-                xterm* | rxvt* | gnome-terminal | konsole | eterm* | wterm | cygwin)  unset tty ;;
-                *);;
-        esac
-
         dir_color=${!dir_color}
         rc_color=${!rc_color}
         virtualenv_color=${!virtualenv_color}
         user_id_color=${!user_id_color}
         root_id_color=${!root_id_color}
-
-        ########################################################### HOST
-        ### we don't display home host/domain  $SSH_* set by SSHD or keychain
-
-        # How to find out if session is local or remote? Working with "su -", ssh-agent, and so on ?
-
-        ## is sshd our parent?
-        # if    { for ((pid=$$; $pid != 1 ; pid=`ps h -o pid --ppid $pid`)); do ps h -o command -p $pid; done | grep -q sshd && echo == REMOTE ==; }
-        #then
-
-        host=${HOSTNAME}
-        if [[ $short_hostname = "on" ]]; then
-			if [[ "$(uname)" =~ "CYGWIN" ]]; then
-				host=`hostname`
-			else
-				host=`hostname -s`
-			fi
-        fi
-        host=${host#$default_host}
-        uphost=`echo ${host} | tr a-z-. A-Z_`
-        if [[ $upcase_hostname = "on" ]]; then
-                host=${uphost}
-        fi
-
-        host_color=${uphost}_host_color
-        host_color=${!host_color}
-        if [[ -z $host_color && -x /usr/bin/cksum ]] ;  then
-                cksum_color_no=`echo $uphost | cksum  | awk '{print $1%6}'`
-                color_index=(green yellow blue magenta cyan white)              # FIXME:  bw,  color-256
-                host_color=${color_index[cksum_color_no]}
-        fi
-
-        host_color=${!host_color}
-
-        # we might already have short host name
-        host=${host%.$default_domain}
-
-#################################################################### WHO_WHERE
-        #  [[user@]host[-tty]]
-
-        if [[ -n $id  || -n $host ]] ;   then
-                [[ -n $id  &&  -n $host ]]  &&  at='@'  || at=''
-                color_who_where="${id}${host:+$host_color$at$host}${tty:+ $tty}"
-                plain_who_where="${id}$at$host"
-
-                # add trailing " "
-                color_who_where="$color_who_where "
-                plain_who_where="$plain_who_where "
-
-                # if root then make it root_color
-                if [ "$id" == "root" ]  ; then
-                        user_id_color=$root_id_color
-                        prompt_char="$root_prompt_char"
-                fi
-                color_who_where="$user_id_color$color_who_where$colors_reset"
-        else
-                color_who_where=''
-        fi
-
-
-parse_svn_status() {
-
-        [[   -d .svn  ]] || return 1
-
-        vcs=svn
-
-        ### get rev
-        eval `
-            svn info |
-                sed -n "
-                    s@^URL[^/]*//@repo_dir=@p
-                    s/^Revision: /rev=/p
-                "
-        `
-        ### get status
-
-        unset status modified added clean init added mixed untracked op detached
-        eval `svn status 2>/dev/null |
-                sed -n '
-                    s/^A...    \([^.].*\)/modified=modified;             modified_files[${#modified_files[@]}]=\"\1\";/p
-                    s/^M...    \([^.].*\)/modified=modified;             modified_files[${#modified_files[@]}]=\"\1\";/p
-                    s/^\?...    \([^.].*\)/untracked=untracked;  untracked_files[${#untracked_files[@]}]=\"\1\";/p
-                '
-        `
-        # TODO branch detection if standard repo layout
-
-        [[  -z $modified ]]   &&  [[ -z $untracked ]]  &&  clean=clean
-        vcs_info=svn:r$rev
- }
-
-parse_hg_status() {
-
-        # ☿
-        hg_root=`hg root 2>/dev/null` || return 1
-
-        vcs=hg
-
-        ### get status
-        unset status modified added clean init added mixed untracked op detached
-
-        eval `hg status 2>/dev/null |
-                sed -n '
-                        s/^M \([^.].*\)/modified=modified; modified_files[${#modified_files[@]}]=\"\1\";/p
-                        s/^A \([^.].*\)/added=added; added_files[${#added_files[@]}]=\"\1\";/p
-                        s/^R \([^.].*\)/added=added;/p
-                        s/^! \([^.].*\)/modified=modified;/p
-                        s/^? \([^.].*\)/untracked=untracked; untracked_files[${#untracked_files[@]}]=\\"\1\\";/p
-        '`
-
-        branch=`hg branch 2> /dev/null`
-
-        [[ -f $hg_root/.hg/bookmarks.current ]] && bookmark=`cat "$hg_root/.hg/bookmarks.current"`
-
-        [[ -z $modified ]]   &&   [[ -z $untracked ]]   &&   [[ -z $added ]]   &&   clean=clean
-        vcs_info=${branch/default/D}
-        if [[ "$bookmark" ]] ;  then
-                vcs_info+=/$bookmark
-        fi
- }
-
 
 
 parse_git_status() {
@@ -422,11 +229,6 @@ parse_git_status() {
         vcs=git
 
         ##########################################################   GIT STATUS
-	added_files=()
-	modified_files=()
-	untracked_files=()
-        [[ $rawhex_len -gt 0 ]]  && freshness="$dim="
-
         unset branch status modified added clean init added mixed untracked op detached
 
 	# info not in porcelain status
@@ -436,9 +238,6 @@ parse_git_status() {
                         s/^# On branch /branch=/p
                         s/^nothing to commi.*/clean=clean/p
                         s/^# Initial commi.*/init=init/p
-                        s/^# Your branch is ahead of \(.\).\+\1 by [[:digit:]]\+ commit.*/freshness=${WHITE}↑/p
-                        s/^# Your branch is behind \(.\).\+\1 by [[:digit:]]\+ commit.*/freshness=${YELLOW}↓/p
-                        s/^# Your branch and \(.\).\+\1 have diverged.*/freshness=${YELLOW}↕/p
                     '
         )"
 
@@ -453,17 +252,6 @@ parse_git_status() {
                                         # git status --porcelain
                                         # A  "with space"                 <------------- WITH QOUTES
 
-        eval " $(
-                git status --porcelain 2>/dev/null |
-                        sed -n '
-                                s,^[MARC]. \([^\"][^/]*/\?\).*,         added=added;           [[ \" ${added_files[@]} \"      =~ \" \1 \" ]]   || added_files[${#added_files[@]}]=\"\1\",p
-                                s,^[MARC]. \"\([^/]\+/\?\).*\"$,        added=added;           [[ \" ${added_files[@]} \"      =~ \" \1 \" ]]   || added_files[${#added_files[@]}]=\"\1\",p
-                                s,^.[MAU] \([^\"][^/]*/\?\).*,          modified=modified;     [[ \" ${modified_files[@]} \"   =~ \" \1 \" ]]   || modified_files[${#modified_files[@]}]=\"\1\",p
-                                s,^.[MAU] \"\([^/]\+/\?\).*\"$,         modified=modified;     [[ \" ${modified_files[@]} \"   =~ \" \1 \" ]]   || modified_files[${#modified_files[@]}]=\"\1\",p
-                                s,^?? \([^\"][^/]*/\?\).*,              untracked=untracked;   [[ \" ${untracked_files[@]} \"  =~ \" \1 \" ]]   || untracked_files[${#untracked_files[@]}]=\"\1\",p
-                                s,^?? \"\([^/]\+/\?\).*\"$,             untracked=untracked;   [[ \" ${untracked_files[@]} \"  =~ \" \1 \" ]]   || untracked_files[${#untracked_files[@]}]=\"\1\",p
-                        '   # |tee /dev/tty
-        )"
 
         if  ! grep -q "^ref:" "$git_dir/HEAD"  2>/dev/null;   then
                 detached=detached
@@ -511,18 +299,6 @@ parse_git_status() {
         fi
 
 
-        ####  GET GIT HEX-REVISION
-        if  [[ $rawhex_len -gt 0 ]] ;  then
-                rawhex=`git rev-parse HEAD 2>/dev/null`
-                rawhex=${rawhex/HEAD/}
-                rawhex="$hex_vcs_color${rawhex:0:$rawhex_len}"
-        else
-                rawhex=""
-        fi
-
-        #### branch
-        branch=${branch/#master/M}
-
                         # another method of above:
                         # branch=$(git symbolic-ref -q HEAD || { echo -n "detached:" ; git name-rev --name-only HEAD 2>/dev/null; } )
                         # branch=${branch#refs/heads/}
@@ -544,7 +320,7 @@ parse_git_status() {
                         fi
                         #branch="<$branch>"
                 fi
-                vcs_info="$branch$freshness$rawhex"
+                vcs_info="$branch"
 
         fi
  }
@@ -552,10 +328,8 @@ parse_git_status() {
 
 parse_vcs_status() {
 
-        unset   file_list modified_files untracked_files added_files
         unset   vcs vcs_info
         unset   status modified untracked added init detached
-        unset   file_list modified_files untracked_files added_files
 
         [[ $vcs_ignore_dir_list =~ $PWD ]] && return
 
@@ -577,54 +351,7 @@ parse_vcs_status() {
                                 # no def:  vcs_color=${vcs_color:-$WHITE}    # default
 
 
-        ### VIM
-
-        if  [[ $vim_module = "on" ]] ;  then
-                # equivalent to vim_glob=`ls .*.vim`  but without running ls
-                unset vim_glob vim_file vim_files
-                old_nullglob=`shopt -p nullglob`
-                    shopt -s nullglob
-                    vim_glob=`echo .*.sw?`
-                eval $old_nullglob
-
-                if [[ $vim_glob ]];  then
-                    set $vim_glob
-                    #vim_file=${vim_glob#.}
-                    if [[ $# > 1 ]] ; then
-                            vim_files="*"
-                    else
-                            vim_file=${1#.}
-                            vim_file=${vim_file/.sw?/}
-                            [[ .${vim_file}.swp -nt $vim_file ]]  && vim_files=$vim_file
-                    fi
-                    # if swap is newer,  then this is unsaved vim session
-                    # [temoto custom] if swap is older, then it must be deleted, so show all swaps.
-                fi
-        fi
-
-
-        ### file list
-        unset file_list
-        if [[ $count_only = "on" ]] ; then
-                [[ ${added_files[0]}     ]]  &&  file_list+=" "${added_vcs_color}+${#added_files[@]}
-                [[ ${modified_files[0]}  ]]  &&  file_list+=" "${modified_vcs_color}*${#modified_files[@]}
-                [[ ${untracked_files[0]} ]]  &&  file_list+=" "${untracked_vcs_color}?${#untracked_files[@]}
-        else
-                [[ ${added_files[0]}     ]]  &&  file_list+=" "$added_vcs_color${added_files[@]}
-                [[ ${modified_files[0]}  ]]  &&  file_list+=" "$modified_vcs_color${modified_files[@]}
-                [[ ${untracked_files[0]} ]]  &&  file_list+=" "$untracked_vcs_color${untracked_files[@]}
-        fi
-        [[ ${vim_files}          ]]  &&  file_list+=" "${MAGENTA}vim:${vim_files}
-
-        if [[ ${#file_list} -gt $max_file_list_length ]]  ;  then
-                file_list=${file_list:0:$max_file_list_length}
-                if [[ $max_file_list_length -gt 0 ]]  ;  then
-                        file_list="${file_list% *} $elipses_marker"
-                fi
-        fi
-
-
-        head_local="$vcs_color(${vcs_info}$vcs_color${file_list}$vcs_color)"
+        head_local="[${vcs_info}$vcs_color$vcs_color]"
 
         ### fringes
         head_local="${head_local+$vcs_color$head_local }"
@@ -643,77 +370,25 @@ parse_virtualenv_status() {
     fi
  }
 
-disable_set_shell_label() {
-        trap - DEBUG  >& /dev/null
- }
-
-# show currently executed command in label
-enable_set_shell_label() {
-        disable_set_shell_label
-	# check for BASH_SOURCE being empty, no point running set_shell_label on every line of .bashrc
-        trap '[[ -z "$BASH_SOURCE" && ($BASH_COMMAND != prompt_command_function) ]] &&
-	     set_shell_label $BASH_COMMAND' DEBUG  >& /dev/null
- }
-
-declare -ft disable_set_shell_label
-declare -ft enable_set_shell_label
-
-# autojump (see http://wiki.github.com/joelthelion/autojump)
-
-# TODO reverse the line order of a file
-#awk ' { line[NR] = $0 }
-#      END  { for (i=NR;i>0;i--)
-#             print line[i] }' listlogs
-
-j (){
-        : ${1? usage: j dir-beginning}
-        # go in ring buffer starting from current index.  cd to first matching dir
-        for (( i=(aj_idx-1)%aj_max;   i != aj_idx%aj_max;  i=(--i+aj_max)%aj_max )) ; do
-                if [[ ${aj_dir_list[$i]} =~ ^.*/$1[^/]*$ ]] ; then
-                        cd "${aj_dir_list[$i]}"
-                        return
-                fi
-        done
-        echo '?'
- }
-
-alias jumpstart='echo ${aj_dir_list[@]}'
-
 ###################################################################### PROMPT_COMMAND
 
 prompt_command_function() {
-        rc="$?"
-
-        if [[ "$rc" == "0" ]]; then
-                rc=""
-        else
-                rc="$rc_color$rc$colors_reset$bell "
-        fi
 
         cwd=${PWD/$HOME/\~}                     # substitute  "~"
-        set_shell_label "${cwd##[/~]*/}/"       # default label - path last dir
 
 	parse_virtualenv_status
         parse_vcs_status
-
-        # autojump
-        if [[ ${aj_dir_list[aj_idx%aj_max]} != $PWD ]] ; then
-              aj_dir_list[++aj_idx%aj_max]="$PWD"
-        fi
 
         # if cwd_cmd have back-slash, then assign it value to cwd
         # else eval cwd_cmd,  cwd should have path after exection
         eval "${cwd_cmd/\\/cwd=\\\\}"
 
-        PS1="$colors_reset$rc$head_local$color_who_where$dir_color$cwd$tail_local$dir_color$prompt_char $colors_reset"
+        #PS1="$colors_reset$rc$head_local$dir_color$cwd$tail_local$dir_color$prompt_char $colors_reset"
+				PS1="$colors_reset$head_local$dir_color$cwd$tail_local$dir_color $prompt_char $colors_reset"
 
-        unset head_local tail_local pwd
+        unset head_local tail_local
  }
 
         PROMPT_COMMAND=prompt_command_function
-
-        enable_set_shell_label
-
-        unset rc id tty modified_files file_list
 
 # vim: set ft=sh ts=8 sw=8 et:
